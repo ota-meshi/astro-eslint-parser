554912da84e24507909d49594f64bb9b048d3aba
yosuke ota <otameshiyo23@gmail.com>
2022-04-28 16:48:37 +0900

    Update

--- .eslintrc-for-playground.js
+++ .eslintrc-for-playground.js
@@ -4,6 +4,11 @@ module.exports = {
     extends: [
         require.resolve("./.eslintrc.js"),
     ],
+    parserOptions: {
+        sourceType: "module",
+        ecmaVersion: 2022,
+        parser: "@typescript-eslint/parser",
+    },
     overrides: [
         {
             files: ["*.astro"],
