e4c0fb43df036e9bc2374c40636d9c970c39889e
yosuke ota <otameshiyo23@gmail.com>
2022-05-27 16:33:53 +0900

    Update eslint-plugin-astro

--- explorer-v3/.eslintrc.cjs
+++ explorer-v3/.eslintrc.cjs
@@ -68,6 +68,14 @@ module.exports = {
       rules: {
         "astro/no-set-html-directive": "error",
         "astro/no-set-text-directive": "error",
+        "astro/prefer-class-list-directive": "error",
+        "astro/prefer-object-class-list": "error",
+        "astro/prefer-split-class-list": [
+          "error",
+          {
+            splitLiteral: true,
+          },
+        ],
         "prettier/prettier": "error",
         "react/jsx-equals-spacing": "error",
 
