c53b8000c91cc2b058a1e2ca20fd55a80a91b211
Yosuke Ota <otameshiyo23@gmail.com>
2022-07-01 21:54:48 +0000

    format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,75 +1,75 @@
-"use strict"
+"use strict";
 
 // const version = require("./package.json").version
 
 module.exports = {
-    parserOptions: {
-        sourceType: "script",
-        ecmaVersion: 2020,
+  parserOptions: {
+    sourceType: "script",
+    ecmaVersion: 2020,
+  },
+  extends: [
+    "plugin:@ota-meshi/recommended",
+    "plugin:@ota-meshi/+node",
+    "plugin:@ota-meshi/+typescript",
+    "plugin:@ota-meshi/+prettier",
+    "plugin:@ota-meshi/+package-json",
+    "plugin:@ota-meshi/+json",
+  ],
+  rules: {
+    "require-jsdoc": "error",
+    "no-warning-comments": "warn",
+    "no-lonely-if": "off",
+    "no-shadow": "off",
+  },
+  overrides: [
+    {
+      files: ["*.ts"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+      rules: {
+        "no-void": ["error", { allowAsStatement: true }],
+        "@typescript-eslint/naming-convention": [
+          "error",
+          {
+            selector: "default",
+            format: ["camelCase"],
+            leadingUnderscore: "allow",
+            trailingUnderscore: "allow",
+          },
+          {
+            selector: "variable",
+            format: ["camelCase", "UPPER_CASE"],
+            leadingUnderscore: "allow",
+            trailingUnderscore: "allow",
+          },
+          {
+            selector: "typeLike",
+            format: ["PascalCase"],
+          },
+          {
+            selector: "property",
+            format: null,
+          },
+          {
+            selector: "method",
+            format: null,
+          },
+        ],
+        "no-implicit-globals": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-use-before-define": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+      },
     },
-    extends: [
-        "plugin:@ota-meshi/recommended",
-        "plugin:@ota-meshi/+node",
-        "plugin:@ota-meshi/+typescript",
-        "plugin:@ota-meshi/+prettier",
-        "plugin:@ota-meshi/+package-json",
-        "plugin:@ota-meshi/+json",
-    ],
-    rules: {
-        "require-jsdoc": "error",
-        "no-warning-comments": "warn",
-        "no-lonely-if": "off",
-        "no-shadow": "off",
+    {
+      files: ["scripts/**/*.ts", "tests/**/*.ts"],
+      rules: {
+        "require-jsdoc": "off",
+        "no-console": "off",
+      },
     },
-    overrides: [
-        {
-            files: ["*.ts"],
-            parser: "@typescript-eslint/parser",
-            parserOptions: {
-                sourceType: "module",
-                project: "./tsconfig.json",
-            },
-            rules: {
-                "no-void": ["error", { allowAsStatement: true }],
-                "@typescript-eslint/naming-convention": [
-                    "error",
-                    {
-                        selector: "default",
-                        format: ["camelCase"],
-                        leadingUnderscore: "allow",
-                        trailingUnderscore: "allow",
-                    },
-                    {
-                        selector: "variable",
-                        format: ["camelCase", "UPPER_CASE"],
-                        leadingUnderscore: "allow",
-                        trailingUnderscore: "allow",
-                    },
-                    {
-                        selector: "typeLike",
-                        format: ["PascalCase"],
-                    },
-                    {
-                        selector: "property",
-                        format: null,
-                    },
-                    {
-                        selector: "method",
-                        format: null,
-                    },
-                ],
-                "no-implicit-globals": "off",
-                "@typescript-eslint/no-non-null-assertion": "off",
-                "@typescript-eslint/no-use-before-define": "off",
-                "@typescript-eslint/no-explicit-any": "off",
-            },
-        },
-        {
-            files: ["scripts/**/*.ts", "tests/**/*.ts"],
-            rules: {
-                "require-jsdoc": "off",
-                "no-console": "off",
-            },
-        },
-    ],
-}
+  ],
+};
