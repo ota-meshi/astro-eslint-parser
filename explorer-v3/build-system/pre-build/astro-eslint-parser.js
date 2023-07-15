c6d2dd7d5829b6f2a87dd558f542e75926f5dcdc
Yosuke Ota <otameshiyo23@gmail.com>
2023-07-15 20:52:50 +0000

    fix

--- explorer-v3/build-system/pre-build/astro-eslint-parser.js
+++ explorer-v3/build-system/pre-build/astro-eslint-parser.js
@@ -1,3 +1,4 @@
 import { parseForESLint } from "astro-eslint-parser";
 export { parseForESLint };
+// eslint-disable-next-line n/no-extraneous-import -- site
 export { setup, parse as parseByCompiler } from "astrojs-compiler-sync";
