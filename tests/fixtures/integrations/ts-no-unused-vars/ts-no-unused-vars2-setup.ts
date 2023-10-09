f649717c92a806fe3ee83718c85bcbf1c9f91f9c
yosuke ota <otameshiyo23@gmail.com>
2023-10-09 22:19:57 +0900

    test: add test case

---
+++ tests/fixtures/integrations/ts-no-unused-vars/ts-no-unused-vars2-setup.ts
@@ -0,0 +1,25 @@
+/* eslint eslint-comments/require-description: 0, @typescript-eslint/explicit-module-boundary-types: 0 */
+import type { Linter } from "eslint";
+import { getBasicParserOptions } from "../../../src/parser/test-utils";
+import { rules } from "@typescript-eslint/eslint-plugin";
+export function setupLinter(linter: Linter) {
+  linter.defineRule(
+    "@typescript-eslint/no-unused-vars",
+    rules["no-unused-vars"] as never,
+  );
+}
+
+export function getConfig() {
+  return {
+    parser: "astro-eslint-parser",
+    parserOptions: getBasicParserOptions(),
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+      "no-unused-vars": "error",
+    },
+    env: {
+      browser: true,
+      es2021: true,
+    },
+  };
+}
