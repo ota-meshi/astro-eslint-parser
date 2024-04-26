7126ccc91e91f8caed2eda1460f47364ebe462cc
Yosuke Ota <otameshiyo23@gmail.com>
2024-04-26 12:13:46 +0900

    fix: crash in scope analysis when using espree (#318)
    * fix: crash in scope analysis when using espree
    
    * fix
    
    * fix
    
    * Create chilly-numbers-smell.md

--- src/parser/script.ts
+++ src/parser/script.ts
@@ -1,10 +1,17 @@
 import type { Context } from "../context";
 import { debug } from "../debug";
-import type { ParserOptionsContext } from "../context/parser-options";
+import type {
+  ParserOptions,
+  ParserOptionsContext,
+} from "../context/parser-options";
 import type { ESLintExtendedProgram } from "../types";
 import { tsPatch } from "./ts-patch";
 import { isEnhancedParserObject } from "../context/resolve-parser/parser-object";
-import { analyze } from "@typescript-eslint/scope-manager";
+import type { ScopeManager } from "@typescript-eslint/scope-manager";
+import { analyze as analyzeForTypeScript } from "@typescript-eslint/scope-manager";
+import { analyze as analyzeForEcmaScript } from "eslint-scope";
+import { KEYS } from "../visitor-keys";
+import { getKeys } from "../traverse";
 /**
  * Parse for script
  */
@@ -17,17 +24,45 @@ export function parseScript(
 
   const parserOptions = parserOptionsCtx.parserOptions;
   if (!result.scopeManager && parserOptions.eslintScopeManager) {
-    result.scopeManager = analyze(result.ast, {
-      ecmaVersion: 1e8,
+    result.scopeManager = analyzeScope(result, parserOptions);
+  }
+
+  return result;
+}
+
+/**
+ * Analyze scope
+ */
+function analyzeScope(
+  result: ESLintExtendedProgram,
+  parserOptions: ParserOptions,
+): ScopeManager {
+  try {
+    return analyzeForTypeScript(result.ast, {
       globalReturn: parserOptions.ecmaFeatures?.globalReturn,
       jsxPragma: parserOptions.jsxPragma,
       jsxFragmentName: parserOptions.jsxFragmentName,
       lib: parserOptions.lib,
       sourceType: parserOptions.sourceType,
     });
+  } catch {
+    // ignore
   }
+  const ecmaFeatures = parserOptions.ecmaFeatures || {};
 
-  return result;
+  return analyzeForEcmaScript(result.ast, {
+    ignoreEval: true,
+    nodejsScope: ecmaFeatures.globalReturn,
+    impliedStrict: ecmaFeatures.impliedStrict as never,
+    ecmaVersion: 1e8,
+    sourceType:
+      parserOptions.sourceType === "commonjs"
+        ? "script"
+        : parserOptions.sourceType || "script",
+    // @ts-expect-error -- Type bug?
+    childVisitorKeys: result.visitorKeys || KEYS,
+    fallback: getKeys,
+  }) as ScopeManager;
 }
 
 /**
