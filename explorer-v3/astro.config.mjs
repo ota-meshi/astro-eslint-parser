8c40f77735179ec06b3182cd4b060ec980454a47
Nikita Karamov <me@kytta.dev>
2026-06-24 21:28:38 +0200

    Remove shim for fast-glob

--- explorer-v3/astro.config.mjs
+++ explorer-v3/astro.config.mjs
@@ -25,7 +25,6 @@ export default defineConfig({
         "astro-eslint-parser": resolve(
           "./build-system/shim/astro-eslint-parser/index.js",
         ),
-        "fast-glob": resolve("./build-system/shim/fast-glob.js"),
         tinyglobby: resolve("./build-system/shim/tinyglobby.js"),
         tslib: resolve("../node_modules/tslib/tslib.es6.js"),
         "escape-string-regexp": resolve(
